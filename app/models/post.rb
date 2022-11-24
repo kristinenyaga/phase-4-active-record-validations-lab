a843f8250193316e19a36711c05125758d6baa18
krisine <kristine.nyaga@student.moringaschool.com>
2022-11-24 17:54:57 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+  validates :content, presence: true,length:{minimum: 250}
+  validates :title, presence: true
+  validates :summary, length:{maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    if !(["Won't Believe","Secret","Top [number]","Guess"].any?{|phrase|"#{title}".include?(phrase)})
+      errors.add(:title, "not clickbaity")
+    end
+  end
 end
+
